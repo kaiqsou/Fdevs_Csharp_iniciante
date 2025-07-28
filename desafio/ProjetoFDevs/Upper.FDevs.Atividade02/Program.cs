662976e991cf5c517fdd4c90c431071479fd1f19
Kaique <kaiqsouza2004@hotmail.com>
2025-07-27 23:31:56 -0300

    Envolvendo códigos por Try Catch

--- desafio/ProjetoFDevs/Upper.FDevs.Atividade02/Program.cs
+++ desafio/ProjetoFDevs/Upper.FDevs.Atividade02/Program.cs
@@ -12,42 +12,50 @@ namespace Upper.FDevs.Atividade02
         // O IMC é calculado pela fórmula:  peso / (altura * altura). Exiba o resultado indicando a faixa de classificação.
         static void Main(string[] args)
         {
-            // obs: é preciso escrever os valores com VÍRGULA por conta da região, senão dá um valor baixíssimo
-            Console.WriteLine("---- Calculador de Massa Corporal (IMC) ----");
-
-            Console.Write("Digite o seu peso (em kg): ");
-            double peso = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite a sua altura (em metros): ");
-            double altura = Convert.ToDouble(Console.ReadLine());
-
-            double imc = Math.Round(peso / (altura * altura), 2); // arredondando para duas casas decimais
-            string classificacao = "";
-
-            Console.WriteLine("Valor do IMC: " + imc);
-
-            if (imc < 18.5)
+            try
             {
-                classificacao = "Abaixo do Peso";
+                // obs: é preciso escrever os valores com VÍRGULA por conta da região, senão dá um valor baixíssimo
+                Console.WriteLine("---- Calculador de Massa Corporal (IMC) ----");
+
+                Console.Write("Digite o seu peso (em kg): ");
+                double peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a sua altura (em metros): ");
+                double altura = Convert.ToDouble(Console.ReadLine());
+
+                double imc = Math.Round(peso / (altura * altura), 2); // arredondando para duas casas decimais
+                string classificacao = "";
+
+                Console.WriteLine("Valor do IMC: " + imc);
+
+                if (imc < 18.5)
+                {
+                    classificacao = "Abaixo do Peso";
+                }
+                else if (imc < 25)
+                {
+                    classificacao = "Peso Normal";
+                }
+                else if (imc < 30)
+                {
+                    classificacao = "Obesidade grau I";
+                }
+                else if (imc <= 40)
+                {
+                    classificacao = "Obesidade grau II";
+                }
+                else
+                {
+                    classificacao = "Obesidade grau III";
+                };
+
+                Console.WriteLine("Classificação: " + classificacao);
             }
-            else if (imc < 25)
+            catch (Exception e)
             {
-                classificacao = "Peso Normal";
+                Console.WriteLine("Erro: " + e);
             }
-            else if (imc < 30)
-            {
-                classificacao = "Obesidade grau I";
-            }
-            else if (imc <= 40)
-            {
-                classificacao = "Obesidade grau II";
-            }
-            else
-            {
-                classificacao = "Obesidade grau III";
-            };
-
-            Console.WriteLine("Classificação: " + classificacao);
+            
         }
     }
 }
--- desafio/ProjetoFDevs/Upper.FDevs.Atividade03/Program.cs
+++ desafio/ProjetoFDevs/Upper.FDevs.Atividade03/Program.cs
@@ -12,75 +12,82 @@ namespace Upper.FDevs.Atividade03
         // O programa deve solicitar dois números e a operação desejada. Exiba o resultado no console.
         static void Main(string[] args)
         {
-            Console.WriteLine("---- Calculadora Simples ----\n");
-
-            Console.Write("Digite o primeiro número: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite o segundo número: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("\nEscolha a operação desejada:\n");
-            Console.WriteLine("1. Soma (+)");
-            Console.WriteLine("2. Subtração (-)");
-            Console.WriteLine("3. Multiplicação (*)");
-            Console.WriteLine("4. Divisão (/)");
-            string operacao = Console.ReadLine();
-            double resultado = 0;
-           
-            // Validando com SWITCH CASE
-            switch (operacao.ToLower())
+            try
             {
-                case "1":
-                case "+":
-                case "soma":
-                    operacao = "Soma";
-                    resultado = Math.Round(num1 + num2, 2);
-                    Console.WriteLine("OPERAÇÃO: " + operacao);
-                    Console.WriteLine("{0} + {1} = " + resultado, num1, num2);
-                    break;
+                Console.WriteLine("---- Calculadora Simples ----\n");
 
-                case "2":
-                case "-":
-                case "subtração":
-                case "subtracao":
-                    operacao = "Subtração";
-                    resultado = Math.Round(num1 - num2, 2);
-                    Console.WriteLine("OPERAÇÃO: " + operacao);
-                    Console.WriteLine("{0} - {1} = " + resultado, num1, num2);
-                    break;
+                Console.Write("Digite o primeiro número: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
 
-                case "3":
-                case "*":
-                case "multiplicação":
-                case "multiplicacao":
-                    operacao = "Multiplicação";
-                    resultado = Math.Round(num1 * num2, 2);
-                    Console.WriteLine("OPERAÇÃO: " + operacao);
-                    Console.WriteLine("{0} x {1} = " + resultado, num1, num2);
-                    break;
+                Console.Write("Digite o segundo número: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
 
-                case "4":
-                case "/":
-                case "divisão":
-                case "divisao":
-                    if (num2 != 0)
-                    {
-                        operacao = "Divisão";
-                        resultado = Math.Round(num1 / num2, 2);
+                Console.WriteLine("\nEscolha a operação desejada:\n");
+                Console.WriteLine("1. Soma (+)");
+                Console.WriteLine("2. Subtração (-)");
+                Console.WriteLine("3. Multiplicação (*)");
+                Console.WriteLine("4. Divisão (/)");
+                string operacao = Console.ReadLine();
+                double resultado = 0;
+
+                // Validando com SWITCH CASE
+                switch (operacao.ToLower())
+                {
+                    case "1":
+                    case "+":
+                    case "soma":
+                        operacao = "Soma";
+                        resultado = Math.Round(num1 + num2, 2);
                         Console.WriteLine("OPERAÇÃO: " + operacao);
-                        Console.WriteLine("{0} / {1} = " + resultado, num1, num2);
+                        Console.WriteLine("{0} + {1} = " + resultado, num1, num2);
                         break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Resultado indefinido. Por favor, informe outro número no lugar de {0}!", num2);
-                        break;
-                    }
 
-                default:
-                    Console.WriteLine("Operação inválida. Por favor, tente novamente.");
-                    break;
+                    case "2":
+                    case "-":
+                    case "subtração":
+                    case "subtracao":
+                        operacao = "Subtração";
+                        resultado = Math.Round(num1 - num2, 2);
+                        Console.WriteLine("OPERAÇÃO: " + operacao);
+                        Console.WriteLine("{0} - {1} = " + resultado, num1, num2);
+                        break;
+
+                    case "3":
+                    case "*":
+                    case "multiplicação":
+                    case "multiplicacao":
+                        operacao = "Multiplicação";
+                        resultado = Math.Round(num1 * num2, 2);
+                        Console.WriteLine("OPERAÇÃO: " + operacao);
+                        Console.WriteLine("{0} x {1} = " + resultado, num1, num2);
+                        break;
+
+                    case "4":
+                    case "/":
+                    case "divisão":
+                    case "divisao":
+                        if (num2 != 0)
+                        {
+                            operacao = "Divisão";
+                            resultado = Math.Round(num1 / num2, 2);
+                            Console.WriteLine("OPERAÇÃO: " + operacao);
+                            Console.WriteLine("{0} / {1} = " + resultado, num1, num2);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Resultado indefinido. Por favor, informe outro número no lugar de {0}!", num2);
+                            break;
+                        }
+
+                    default:
+                        Console.WriteLine("Operação inválida. Por favor, tente novamente.");
+                        break;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro: " + e);
             }
         }
     }
